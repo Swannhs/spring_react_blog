18909c412e35b6807eb1d785d5acbd8169abea54
Swannhs <princeapurbask@gmail.com>
2021-09-09 17:45:34 +0600

    initialize spring boot and react project

---
+++ src/main/java/com/swann/SwannApplication.java
@@ -0,0 +1,13 @@
+package com.swann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwannApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwannApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swann/SwannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwannApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
