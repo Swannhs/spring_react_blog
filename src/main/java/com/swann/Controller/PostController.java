bdbe105f8189ff4ed393242c91b0047441960161
Swannhs <princeapurbask@gmail.com>
2021-09-09 18:45:04 +0600

    Handling error in professional way

--- src/main/java/com/swann/Controller/PostController.java
+++ src/main/java/com/swann/Controller/PostController.java
@@ -5,10 +5,16 @@ import com.swann.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post")
@@ -17,7 +23,15 @@ public class PostController {
     private PostService postService;
 
     @PostMapping
-    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+    public ResponseEntity<?> createPost(@Valid @RequestBody Post post, BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        }
         Post created = postService.saveOrUpdate(post);
         return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
