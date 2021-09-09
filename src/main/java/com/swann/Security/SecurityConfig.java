3125527b3d8a216f7427129c9320ae21d5859b9f
Swannhs <princeapurbask@gmail.com>
2021-09-10 01:34:56 +0600

    Added simple spring security configuration

---
+++ src/main/java/com/swann/Security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.swann.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtAuthenticationEntryPoint unAuthHandler;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unAuthHandler).and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+//                .antMatchers("/api/v1/post")
+//                .permitAll()
+                .antMatchers("/api/auth")
+                .permitAll()
+                .anyRequest().authenticated();
+    }
+}
