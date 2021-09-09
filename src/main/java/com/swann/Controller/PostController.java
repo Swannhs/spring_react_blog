1a685c3c1a84d16340e8228d5619ca856f6b2798
Swannhs <princeapurbask@gmail.com>
2021-09-09 18:54:41 +0600

    Configured error as a custom error handling

--- src/main/java/com/swann/Controller/PostController.java
+++ src/main/java/com/swann/Controller/PostController.java
@@ -2,19 +2,17 @@ package com.swann.Controller;
 
 import com.swann.Model.Post;
 import com.swann.Service.PostService;
+import com.swann.Service.ValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post")
@@ -22,16 +20,16 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private ValidationErrorService errorService;
+
     @PostMapping
     public ResponseEntity<?> createPost(@Valid @RequestBody Post post, BindingResult result) {
-        if (result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for (FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> error = errorService.ErrorService(result);
+        if (error != null){
+            return error;
         }
+
         Post created = postService.saveOrUpdate(post);
         return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
