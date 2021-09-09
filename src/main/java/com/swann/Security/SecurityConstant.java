60a0c5ef50a10bd030f6720bcd5e86e37e1085f0
Swannhs <princeapurbask@gmail.com>
2021-09-10 03:12:36 +0600

    Added some static variables

---
+++ src/main/java/com/swann/Security/SecurityConstant.java
@@ -0,0 +1,8 @@
+package com.swann.Security;
+
+public class SecurityConstant {
+    public static final String SECRET = "SECRETKEYTOKEN";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION_TIME = 300000;
+}
