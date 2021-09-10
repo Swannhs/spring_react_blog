9797c86665759de330e3beaf2a536b044f9930fa
Swannhs <princeapurbask@gmail.com>
2021-09-10 17:08:46 +0600

    Render post as web view

---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import axios from "axios";
+import Post from "../Components/Post";
+import Layout from "../Components/Layout";
+
+class Home extends Component {
+    state = {
+        data: []
+    }
+    onApiCall = () => {
+        axios.get('http://localhost:8080/api/v1/post')
+            .then(response => {
+                this.setState({
+                    data: response.data.data
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.onApiCall();
+    }
+
+    render() {
+        return (
+            <Layout>
+                {
+                    this.state.data.map(post => {
+                        return <Post id={post.postId} title={post.title} content={post.content} createdAt={post.createdAt}/>
+                    })
+                }
+            </Layout>
+        );
+    }
+}
+
+export default Home;
