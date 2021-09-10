775468037ecff0efd7c1d91500174493f791f59b
Swannhs <princeapurbask@gmail.com>
2021-09-10 14:58:32 +0600

    Added simple authentication system implementation

---
+++ src/main/java/com/swann/Service/UserDetailsServiceImp.java
@@ -0,0 +1,41 @@
+package com.swann.Service;
+
+import com.swann.Model.User;
+import com.swann.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import static java.util.Collections.singletonList;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String ussername) throws UsernameNotFoundException {
+        Optional<User> byUsername = userRepository.findByUsername(ussername);
+        User user = byUsername
+                .orElseThrow(() -> new UsernameNotFoundException(ussername + " not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                true, true, true, true,
+                getAuthorities("USER")
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return singletonList(new SimpleGrantedAuthority(role));
+    }
+
+}
