11da35c843268607a246c001b49c18a74865362b
Swannhs <princeapurbask@gmail.com>
2021-09-10 15:11:42 +0600

    Added login route and implement all configuration

--- src/main/java/com/swann/Controller/UserController.java
+++ src/main/java/com/swann/Controller/UserController.java
@@ -1,10 +1,17 @@
 package com.swann.Controller;
 
+import com.swann.Payload.JwtLoginSuccess;
+import com.swann.Payload.LoginRequest;
 import com.swann.Model.User;
+import com.swann.Security.JwtTokenProvider;
 import com.swann.Service.UserService;
 import com.swann.Service.ValidationErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+
+import static com.swann.Security.SecurityConstant.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -22,10 +31,34 @@ public class UserController {
     @Autowired
     private ValidationErrorService errorService;
 
-    @PostMapping
-    public ResponseEntity<?> register(@Valid @RequestBody User user, BindingResult result){
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
         ResponseEntity<?> error = errorService.ErrorService(result);
-        if (error != null){
+        if (error != null) {
+            return error;
+        }
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JwtLoginSuccess(true, jwt));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody User user, BindingResult result) {
+        ResponseEntity<?> error = errorService.ErrorService(result);
+        if (error != null) {
             return error;
         }
         return userService.saveOrUpdate(user);
