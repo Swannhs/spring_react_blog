fa841801a5793968d954ed8969a550340d21fa3e
Swannhs <princeapurbask@gmail.com>
2021-09-10 17:07:40 +0600

    Allow origin to get data

--- src/main/java/com/swann/Controller/PostController.java
+++ src/main/java/com/swann/Controller/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin("http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/post")
 public class PostController {
