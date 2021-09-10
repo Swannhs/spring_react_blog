f7b475acf90e0f3f1e90b90189193d32331a22c4
Swannhs <princeapurbask@gmail.com>
2021-09-10 16:53:36 +0600

    added react router dom and configured for dynamic posts

--- frontend/src/Components/Post.js
+++ frontend/src/Components/Post.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
-const Post = () => {
+const Post = ({id, title, content, createdAt}) => {
     return (
         <div className="card text-center">
-            <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+            <div className="card-body" key={id}>
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{content}</p>
+                <Link to={`/${id}`} className="btn btn-primary">Read</Link>
+                <div className="card-footer text-muted">
+                    {createdAt}
+                </div>
             </div>
         </div>
     );
