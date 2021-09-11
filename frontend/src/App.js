f353602970d5e4c1274dce6838d9fc5478dc6057
Swannhs <princeapurbask@gmail.com>
2021-09-11 17:21:57 +0600

    Rapping store in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,22 @@ import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import {Provider} from "react-redux";
+import store from "./store";
 
 function App() {
     return (
-        <div className="App">
-            <Router>
-                <Switch>
-                    <Route exact path='/' component={Home}/>
-                </Switch>
-            </Router>
-        </div>
+        <Provider store={store}>
+            <div className="App">
+                <Router>
+                    <Switch>
+                        <Route exact path='/' component={Home}/>
+                        <Route exact path='/login' component={Login}/>
+                    </Switch>
+                </Router>
+            </div>
+        </Provider>
     );
 }
 
