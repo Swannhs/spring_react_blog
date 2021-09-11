4bd2d1eaccedffc25c48b8312ee7b56fe8f33e37
Swannhs <princeapurbask@gmail.com>
2021-09-11 17:30:21 +0600

    Removed direct axios request and calling from redux reducer also render posts

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,31 +1,23 @@
 import React, {Component} from 'react';
-import axios from "axios";
-import Posts from "../Components/Posts";
 import Layout from "../Components/Layout";
+import {connect} from "react-redux";
+import {getPosts} from "../Actions/PostAction";
+import PropTypes from 'prop-types';
+import Posts from "../Components/Posts";
 
 class Home extends Component {
-    state = {
-        data: []
-    }
-    onApiCall = () => {
-        axios.get('http://localhost:8080/api/v1/post')
-            .then(response => {
-                this.setState({
-                    data: response.data.data
-                })
-            })
-    }
-
     componentDidMount() {
-        this.onApiCall();
+        this.props.getPosts();
     }
 
     render() {
+        const posts = this.props.posts.posts;
         return (
             <Layout>
                 {
-                    this.state.data.map(post => {
-                        return <Posts id={post.postId} title={post.title} content={post.content} createdAt={post.createdAt}/>
+                    posts.map(post => {
+                        return <Posts id={post.postId} title={post.title} content={post.content}
+                                      createdAt={post.createdAt}/>
                     })
                 }
             </Layout>
@@ -33,4 +25,16 @@ class Home extends Component {
     }
 }
 
-export default Home;
+Home.propsTypes = {
+    posts: PropTypes.object.isRequired,
+    getPosts: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    posts: state.posts
+})
+
+export default connect(
+    mapStateToProps,
+    {getPosts}
+)(Home);
